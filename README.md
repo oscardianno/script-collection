b389c6d608a72b6b1f87972bf82dd1a7dd0a72e0
Oscar Mora <oscare.ramirez@alumnos.udg.mx>
2023-06-14 16:01:22 -0600

    Add sync-notes Python script

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This is a personal collection of scripts I often find useful. Most of them were 
 - **create-virtual-disk**: create a 2GB RAM disk using ImDisk.
 - **get-lock-screen-wallpapers**: batch script to gather Windows 10's lock screens wallpapers from their temporary folders.
 - **no-admin**: drag and drop a program on this to skip the requirement of giving admin rights to it. Some programs will still ask for it.
+- **sync-notes**: script to backup or restore Simple Sticky Notes database file (Notes.db) to another folder for automatic synchronization through SyncTrayzor.
 - **PhoenixDaemon**: it's a VBS script that keeps launching PhoenixMiner (and the overclock tool for AMD cards called OverdriveNTool) when it detects it closed. Useful when a card or the miner is failing, so it re-launches it automatically. Requires to edit and set the paths for such executables.
 - **vlc-multitrack**: drag and drop a video file on this to open it with VLC and playback multiple audio tracks of it at the same time. Useful when, for example, reproducing a video recorded with NVIDIA GeForce Experience with the Separate audio tracks option enabled.
 - **wa-replay-res**: drag and drop a Worms Armageddon replay file to open it with a custom resolution (by default 800x600). Useful for recording plays. It restores the previous resolution after closing. Made by [@cgarz](https://github.com/cgarz)
---
+++ sync-notes.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import psutil
+import shutil
+import subprocess
+
+USER_PROFILE_PATH = os.environ["USERPROFILE"]
+SOURCE_PATH = USER_PROFILE_PATH + "\\Documents\\Simple Sticky Notes\\"
+BACKUP_PATH = USER_PROFILE_PATH + "\\Documents\\Synced Notes\\"
+FILE_NAME = "Notes.db"
+APP_PATH = "C:\\Program Files (x86)\\Simnet\\Simple Sticky Notes\\"
+APP_EXE_NAME = "ssn.exe"
+
+
+def kill_app(name):
+    for proc in psutil.process_iter():
+        if proc.name() == name:
+            proc.kill()
+
+
+def save():
+    print("Saving...")
+    try:
+        # If "Synced Notes" directory does not exist, create it
+        if not os.path.exists(BACKUP_PATH):
+            os.makedirs(BACKUP_PATH)
+
+        # Copy "Notes.db" file from "Documents\Simple Sticky Notes" directory to "Documents\Synced Notes" directory
+        shutil.copy2(SOURCE_PATH + FILE_NAME, BACKUP_PATH + FILE_NAME)
+        print("Save successful!")
+    except IOError as e:
+        print("Save failed: " + e)
+
+
+def load():
+    print("Loading...")
+    try:
+        # Close the Simple Sticky Notes application
+        os.system("taskkill /f /im " + APP_EXE_NAME)
+        # Backup: Copy and rename the "Notes.db" file in "Documents\Simple Sticky Notes" directory to "Notes.db.bak"
+        shutil.copy2(SOURCE_PATH + FILE_NAME, SOURCE_PATH + FILE_NAME + ".bak")
+        # Copy "Notes.db" file from "Documents\Synced Notes" directory to "Documents\Simple Sticky Notes" directory
+        shutil.copy2(BACKUP_PATH + FILE_NAME, SOURCE_PATH + FILE_NAME)
+        # Open the Simple Sticky Notes application again
+        os.startfile(APP_PATH + APP_EXE_NAME)
+        print("Load successful!")
+    except IOError as e:
+        print("Load failed: " + e)
+
+
+# Receive parameter from command line; "save" or "load" and call the appropriate function
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "save":
+            save()
+        elif sys.argv[1] == "load":
+            load()
+        else:
+            print("Invalid parameter. Please use 'save' or 'load'")
+    else:
+        print("No parameter received. Please use 'save' or 'load'")
